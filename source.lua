decbfa62f54c711ce1cc241e13d7b35e48daa944
ijustwannabeblack <x.m.as.fly33@gmail.com>
2026-07-12 23:22:43 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -29,9 +29,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    MainColor = Color3.fromRGB(40, 20, 60);
+    BackgroundColor = Color3.fromRGB(30, 15, 45);
+    AccentColor = Color3.fromRGB(128, 0, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
